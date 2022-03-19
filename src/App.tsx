d4d5c3be433efe338d3e217d7e5bc5dc8a3b5b94
dejwi <misti.krasula@gmail.com>
2022-03-19 15:53:26 +0100

    base for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles/main.scss';
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div>
-
+      <Navbar/>
     </div>
   );
 }
