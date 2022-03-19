e27e5b7ae442b8efe4ad191ac6e2e9140aea7dfb
dejwi <misti.krasula@gmail.com>
2022-03-19 21:20:36 +0100

    firebase

---
+++ src/components/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth';
+
+import firebase_config from '../firebase-config.json';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebase_config);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
