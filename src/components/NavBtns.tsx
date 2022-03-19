93c07f67f810cd835f044ada21f4f955f02bb8b5
dejwi <misti.krasula@gmail.com>
2022-03-19 17:02:15 +0100

    profile context menu base

--- src/components/NavBtns.tsx
+++ src/components/NavBtns.tsx
@@ -1,8 +1,11 @@
-
+import React, {useState} from "react";
+import ProfMenu from './ProfMenu';
 
 export default function NavBtns(props: { selected: 'home' | 'direct' | 'post' }) {
     const sel = props.selected;
-    const discolor = '#7a7a7a';
+    const [showProfMenu,setShowProfMenu] = useState(false);
+
+
     return (<div className='btns'>
 
         {sel === 'home' ?
@@ -82,6 +85,12 @@ export default function NavBtns(props: { selected: 'home' | 'direct' | 'post' })
         {/*        d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>*/}
         {/*</svg>*/}
 
-        <img src='https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg'/>
+        <div className='profCont' >
+            <picture className={showProfMenu ? 'active' : ''}
+                     onClick={() => setShowProfMenu(!showProfMenu)}>
+                <img src='https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg'/>
+            </picture>
+            { showProfMenu ? <ProfMenu/> : null}
+        </div>
     </div>);
 }
